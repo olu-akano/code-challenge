7f93056f446ccec2e553412dba988ad1cbdb58ee
Max Mackie <80007111+maxmackie13@users.noreply.github.com>
2021-09-28 14:27:03 +0100

    Update script.js

--- client/assets/script.js
+++ client/assets/script.js
@@ -1,19 +1,1 @@
-const form = document.querySelector('form')
-fakeFetchBecauseICantMakeItWord = '"YESTERDAY I WAS CLEVER, SO I CHANGED THE WORLD. TODAY I AM WISE, SO I AM CHANGING MYSELF." -RUMI';
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-  const quote = document.querySelector('#text')
-
-  fetch('http://localhost:3000/quotes/random')
-    .then(res => {
-      return res.json()
-    })
-    .then(data => {
-      console.log('data parsed', data)
-    })
-    .then(quote.innerHTML = fakeFetchBecauseICantMakeItWord)
-    .catch(console.log('Error'))
-});
-
 
