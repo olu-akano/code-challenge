87ea6e40c0d85ce1380e7cad4c15117b95250b23
Oluwaseun Akano <90205470+olu-akano@users.noreply.github.com>
2021-09-28 16:48:26 +0100

    setup server

--- server/app.js
+++ server/app.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 5501;
 
+app.listen(port, () => console.log(`Express is running on port ${port}`))
+
+const cors = require('cors');
+app.use(cors());
