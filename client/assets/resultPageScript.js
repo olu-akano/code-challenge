7b4baab99209351163c54cfa77d6417c6d28247c
Oluwaseun Akano <90205470+olu-akano@users.noreply.github.com>
2021-09-29 11:57:33 +0100

    add DOM for luckybutton

--- client/assets/resultPageScript.js
+++ client/assets/resultPageScript.js
@@ -1,1 +1,17 @@
+const luckyButton = document.getElementById('#lucky');
 
+luckyButton.addEventListener('click', e => {
+    e.preventDefault();
+
+})
+
+
+const imFeelingLucky = async () => {
+    try {
+        let resp = await fetch('http://localhost:3001/lucky')
+        let data = await resp.json();
+        console.log(data)
+    } catch (err) {
+        console.error(`Oh no..${err}`)
+    }
+}
--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3001;
+app.use(cors());
 
 app.listen(port, () => console.log(`Express is running on port ${port}`))
 
-const cors = require('cors');
-app.use(cors());
 
 const dogs = [
     {id: 1, title: 'Dogs Trust: Dogs Rehoming & Dog Rescue Charity', url: 'https://www.dogstrust.org.uk/', description: "Dogs Trust is the UK's largest Dog Welfare Charity. Looking to rehome a rescue dog or to donate to an animal charity? Visit us today to find out more!"},
@@ -14,23 +14,11 @@ const dogs = [
     {id: 4, title: 'Dog, facts and photos - National Geographic', url : 'https://www.nationalgeographic.com/animals/mammals/facts/domestic-dog', description: 'The term “domestic dog” refers to any of several hundred breeds of dog in the world today.'},
     {id: 5, title: "All Dog Breeds - Complete List of Dog Profiles - DogTime", url: "https://dogtime.com/dog-breeds/profiles", description: 'Detailed profiles of more than 200 dog breeds. Includes personality, history, dog pictures, dog health info, and more.'},
     {id: 6, title: 'Dogs | Healthy Pets, Healthy People | CDC', url: "https://www.cdc.gov/healthypets/pets/dogs.html", description: 'Dogs can have many positive effects on the lives of their owners. They influence social, emotional, and cognitive development in children.'},
-    {id: 7, title: "Dogs for Good - Who we are - Assistance Dogs", url = "https://www.dogsforgood.org/", description: "Dogs for Good is a life-transforming charity, creating partnerships between people living with disability and specially trained assistance dogs."},
+    // {id: 7, title: "Dogs for Good - Who we are - Assistance Dogs", url = "https://www.dogsforgood.org/", description: "Dogs for Good is a life-transforming charity, creating partnerships between people living with disability and specially trained assistance dogs."},
     {id: 8, title: 'Here are the 10 most loyal breeds of adorable and cuddly dog that are completely devoted to their owners', url: 'https://www.scotsman.com/lifestyle/family-and-parenting/these-are-the-10-most-loyal-breeds-of-adorable-dog-that-are-completely-devoted-to-their-human-families-3326796', description: ' With demand for puppies remaining high post-lockdown, here are the breeds of dogs that will love their human family unconditionally.'},
     {id: 9, title: 'Birmingham Dogs Home', url: 'https://www.birminghamdogshome.org.uk/?gclid=CjwKCAjwndCKBhAkEiwAgSDKQXu_MCPHT4RNnuUmd4b1-Fab4-qc2DvlxVaU5GHY1o0IdGN0k8jszBoCjggQAvD_BwE', description:'Helping Stray And Rejected Dogs Find Happier Tomorrows. We Provide Rescue & Rehoming Services For Dogs. Support Us Today! Save An Unloved Dog. End Dog Suffering'},
     {id: 10, title: "Manchester and Cheshire Dogs' Home", url: 'https://www.dogshome.net/', description: 'Dogs. Click here to find out more about some of our residents... Fundraise.'}
 ]
-
-app.get('/dogs', (req, res) => res.send(dogs));
-
-
-let luckyItems = [dogs, sport, europe];
-
-function imFeelingLucky () {
-    return luckyItems[Math.floor(Math.random()*items.length)];
-}
-
-app.get('/lucky', (req, res) => res.send(imFeelingLucky));
-
 
 const sport = [
     {id: 1, url: "https://www.bbc.co.uk/sport", title: "Home - BBC Sport", description: "Breaking news & live sports coverage including results, video, audio and analysis on Football, F1, Cricket, Rugby Union, Rugby League, Golf, Tennis and all ..."},
@@ -58,3 +46,42 @@ const europe = [
     {id: 10, url: "https://www.reuters.com/world/europe/", title: "Europe News | Latest Headlines & Stories | Reuters", description: "... future on Tuesday as lawmakers in his CDU/CSU alliance met to decide what to do after suffering their first national election defeat since 2002. Europe ... "}
 ]
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+const getLucky = () => {
+    let searches = [dogs, sport, europe];
+    let randSearch = searches[getRandomInt(0, (searches.length-1))];
+    let randomSiteUrl = randSearch[getRandomInt(0, (randSearch.length-1))].url;
+    return randomSiteUrl
+};
+
+
+app.get('/', (req, res)=> {
+    res.send('Hello there!');
+});
+
+app.get('/dogs', (req, res) => {
+    res.send(dogs)
+});
+
+app.get('/sport', (req, res)=> {
+    res.send(sport);
+});
+
+app.get('/europe', (req, res) => {
+    res.send(europe);
+});
+
+app.get('/lucky', (req, res) => {  
+    let url = getLucky();
+    res.send(url);
+});
+
+
+app.post('/', (req, res) => {
+    res.status(405).send('Not allowd');
+});
